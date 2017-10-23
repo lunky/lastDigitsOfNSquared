3d775242442688f3e0203ff15402476039850d60
Quinn Wilson <c9@midworld.org>
2017-10-23 22:57:34 +0000

    working

--- src/Lib.hs
+++ src/Lib.hs
@@ -10,8 +10,8 @@ module Lib
 import Data.List
 
 testGreen :: Integer -> Bool
---testGreen n = (show n) `isSuffixOf` (show $ n^2)
-testGreen n = n^2 `mod` (len n) == 0
+testGreen n = (n == endsWith)  
+  where   endsWith = (n^2) `mod` (10^(len n))
 
 green :: Int -> Integer
 green n = head $ drop (n-1) $  green' n
@@ -32,10 +32,11 @@ merge (a:as) (b:bs)
   | a < b     = a : merge as (b:bs)
   | a > b     = b : merge (a:as) bs
   | otherwise = a : merge as bs      
-
-    
-len:: (Integral a, Integral a1) => a1 -> a
-len n = floor(logBase 10 ( fromIntegral ( abs n ))) + 1
+  
+--len:: (Integral a, Integral a1) => a1 -> a
+--len n = floor(logBase 10 ( ( abs n ))) + 1
+len :: Show a => a -> Int
+len n = length $ show n
 
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
